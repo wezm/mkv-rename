f427b5769975838f40c13cdbaa38852532e96f45
Wesley Moore <wes@wezm.net>
2023-04-12 15:58:40 +1000

    Allow time offset to be supplied

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,41 @@ use std::process::ExitCode;
 
 use matroska::{Matroska, TagValue};
 use mp4::Mp4Reader;
-use time::format_description::well_known::Iso8601;
-use time::OffsetDateTime;
+use time::format_description::well_known::{Iso8601, Rfc2822};
+use time::{Duration, OffsetDateTime};
+
+struct Flags {
+    dry_run: bool,
+    /// Offset in seconds
+    offset: Duration,
+}
 
 fn main() -> ExitCode {
-    let flags = xflags::parse_or_exit! {
+    let all_flags = xflags::parse_or_exit! {
         /// Don't rename files, just print what would be done
         optional -n,--dry-run
+        /// Offset in hours (can be fractional) to add to timestamps read from file
+        ///
+        /// Some cameras appear to store the creation date in local time, without a timezone.
+        /// This flag allows those times to be adjusted.
+        optional -t,--tz-offset offset: f32
         /// Files to process
         repeated paths: PathBuf
     };
+    // TODO: use xflags::xflags! macro and make this a TryFrom impl
+    let Some(offset) = f32_to_i32((all_flags.tz_offset.unwrap_or_default() * 60. * 60.).round()) else {
+        eprintln!("offset too big");
+        return ExitCode::FAILURE;
+    };
+    let flags = Flags {
+        dry_run: all_flags.dry_run,
+        offset: Duration::new(i64::from(offset), 0),
+    };
 
     let mut ok = true;
-    for f in flags.paths {
+    for f in all_flags.paths {
         let path = Path::new(&f);
-        match process(path, flags.dry_run) {
+        match process(path, &flags) {
             Ok(()) => (),
             Err(err) => {
                 eprintln!("Error processing {}: {}", path.display(), err);
@@ -37,47 +57,59 @@ fn main() -> ExitCode {
     }
 }
 
-fn process(path: &Path, dry_run: bool) -> Result<(), String> {
+fn process(path: &Path, flags: &Flags) -> Result<(), String> {
     match path
         .extension()
         .map(|ext| ext.to_string_lossy().to_ascii_lowercase())
         .as_deref()
     {
-        Some("mkv") => process_matroska(path, dry_run),
-        Some("mov" | "mp4" | "m4v") => process_mp4(path, dry_run),
+        Some("mkv") => process_matroska(path, flags),
+        Some("mov" | "mp4" | "m4v") => process_mp4(path, flags),
         _ => Err(String::from("unknown file type")),
     }
 }
 
-fn process_matroska(path: &Path, dry_run: bool) -> Result<(), String> {
+fn process_matroska(path: &Path, flags: &Flags) -> Result<(), String> {
     let mkv = matroska::open(path).map_err(|err| err.to_string())?;
-    let datetime =
-        mkv_creation_date(&mkv).ok_or_else(|| String::from("unable to determine creation date"))?;
+    let datetime = mkv_creation_date(&mkv)
+        .ok_or_else(|| String::from("unable to determine creation date"))?
+        + flags.offset;
 
     let new_path = generate_new_path(path, datetime);
-    maybe_do_rename(path, &new_path, dry_run)?;
+    println!(
+        "{} -> {} ({})",
+        path.display(),
+        new_path.display(),
+        datetime.format(&Rfc2822).unwrap()
+    );
+    maybe_do_rename(path, &new_path, flags.dry_run)?;
 
     Ok(())
 }
 
-fn process_mp4(path: &Path, dry_run: bool) -> Result<(), String> {
+fn process_mp4(path: &Path, flags: &Flags) -> Result<(), String> {
     let f = File::open(path).map_err(|err| err.to_string())?;
     let size = f.metadata().map_err(|err| err.to_string())?.len();
     let reader = BufReader::new(f);
     let mp4 = Mp4Reader::read_header(reader, size).map_err(|err| err.to_string())?;
-    let datetime =
-        mp4_creation_date(&mp4).ok_or_else(|| String::from("unable to determine creation date"))?;
+    let datetime = mp4_creation_date(&mp4)
+        .ok_or_else(|| String::from("unable to determine creation date"))?
+        + flags.offset;
 
     let new_path = generate_new_path(path, datetime);
-    maybe_do_rename(path, &new_path, dry_run)?;
+    println!(
+        "{} -> {} ({})",
+        path.display(),
+        new_path.display(),
+        datetime.format(&Rfc2822).unwrap()
+    );
+    maybe_do_rename(path, &new_path, flags.dry_run)?;
 
     Ok(())
 }
 
 fn maybe_do_rename(path: &Path, new_path: &PathBuf, dry_run: bool) -> Result<(), String> {
-    if dry_run {
-        println!("{} -> {}", path.display(), new_path.display());
-    } else {
+    if !dry_run {
         fs::rename(path, &new_path)
             .map_err(|err| format!("unable to rename to {}: {}", new_path.display(), err))?;
     }
@@ -116,6 +148,10 @@ fn generate_new_path(path: &Path, creation_date: OffsetDateTime) -> PathBuf {
     file_name.push(" ");
     file_name.push(path.file_name().unwrap()); // file_name should exist at this point
     path.with_file_name(file_name)
+}
+
+fn f32_to_i32(x: f32) -> Option<i32> {
+    (x == (x as i32) as f32).then(|| x as i32)
 }
 
 #[cfg(test)]
